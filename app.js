8d5c9e64c78ea19681ec765d2808b411b64d5cf2
=noja <noelnoja8@gmail.com>
2020-04-28 00:49:59 +0300

    Updates

---
+++ app.js
@@ -0,0 +1,46 @@
+// Menu Btn
+document
+  .querySelector(".menu-btn")
+  .addEventListener("click", () =>
+    document.querySelector(".nav__list").classList.toggle("show")
+  );
+
+// Portfolio
+const portfolioItems = document.querySelectorAll(".portfolio__item");
+portfolioItems.forEach((portfolioItem) => {
+  portfolioItem.addEventListener("mouseover", () => {
+    console.log(portfolioItem.childNodes[1].classList);
+    portfolioItem.childNodes[1].classList.add("image-blur");
+  });
+
+  portfolioItem.addEventListener("mouseout", () => {
+    console.log(portfolioItem.childNodes[1].classList);
+    portfolioItem.childNodes[1].classList.remove("image-blur");
+  });
+});
+
+// Scroll Btn
+const backTop = document.querySelector(".btn__scroll--top");
+backTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 600) {
+    backTop.style.display = "block";
+  } else {
+    backTop.style.display = "none";
+  }
+});
+
+// Colors
+const loopAcccent = function () {
+  const colors = ["#dd2c00", "#ff5722", "#16e0bd", "#00bcd4"];
+  const randomNum = Math.floor(Math.random() * colors.length);
+  document.documentElement.style.setProperty(
+    "--accent-color",
+    colors[randomNum]
+  );
+};
+
+setInterval(loopAcccent, 20000);
